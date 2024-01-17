40df97c10bfd96e7d701ba29f8d13088fc36d9f6
Temuujin undefined <23LP5268@G401-CC0096.local>
2024-01-17 16:03:01 +0800

    1

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+export const Header = () => {
+  return (
+    <div>
+      <p>
+        "<SS />"
+      </p>
+      <h3>About</h3>
+      <h3>Work</h3>
+      <h3>Testimonials</h3>
+      <h3>Contact</h3>
+    </div>
+  );
+};
---
+++ src/pages/portfolio.js
@@ -0,0 +1,17 @@
+import {
+  About,
+  Contactme,
+  Experience,
+  Footer,
+  Header,
+  Hero,
+  Skills,
+  Work,
+} from "@/components";
+export const Portfolio = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
